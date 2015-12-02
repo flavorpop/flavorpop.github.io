240e9f34d9c5d70f5db91bf1593246e72cdc0181
Claire <lee.claire@berkeley.edu>
2015-12-02 12:00:10 -0800

    add jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
 
-
-});
+  $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if (scroll >=600) {
+          // $("#topbar").addClass("color");
+          $("#topbar").css("background-color","#d3d3d3");
+        } else {
+          $("#topbar").removeAttr("background-color");
+        }
+      })
+  });
