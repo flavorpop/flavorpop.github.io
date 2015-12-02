5592d575d8e48ad4b917fffa9026fd159c0c9289
flavorpop <claireannlee@gmail.com>
2015-12-02 12:54:01 -0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,8 @@ $(document).ready(function() {
         var scroll = $(window).scrollTop();
         if (scroll >=600) {
           // $("#topbar").addClass("color");
-          $("#topbar").css("background-color","#d3d3d3");
+          // $("#topbar").css("background-color","#d3d3d3");
+          $("#topbar").css("color","#696969");
         } else {
           $("#topbar").removeAttr("background-color");
         }
